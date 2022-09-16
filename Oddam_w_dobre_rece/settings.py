d883db64bd20c7ed591ad2f7ba73ee4233dd844e
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-09-16 15:43:09 +0200

    language + time zone

--- Oddam_w_dobre_rece/settings.py
+++ Oddam_w_dobre_rece/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
