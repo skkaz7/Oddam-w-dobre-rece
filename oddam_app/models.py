af149d63259cd0ad6e1ab272b5fe49577cc941cc
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-09-17 16:37:31 +0200

    #1 model category + model institution + model donation

--- oddam_app/models.py
+++ oddam_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+TYPE = (
+    (1, 'Fundacja'),
+    (2, 'Organizacja pozarządowa'),
+    (3, 'Zbiórka lokalna')
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE, default=TYPE[0])
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.TextField(max_length=64)
+    phone_number = models.IntegerField(max_length=16)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=16)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default='null', on_delete=models.CASCADE)
