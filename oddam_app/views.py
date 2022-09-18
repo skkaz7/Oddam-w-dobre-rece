131c881b705aa7678f90c4996c7a7afc18e5f535
twój login do GitHuba <s.k.kazmierczak7@gmail.com>
2022-09-18 17:53:54 +0200

    #2 Strona główna - Dynamiczne ładowanie instytucji

--- oddam_app/views.py
+++ oddam_app/views.py
@@ -9,10 +9,16 @@ from oddam_app.models import Donation, Institution, Category
 
 class LandingPage(View):
     def get(self, request):
+        funds = Institution.objects.filter(type=1)
+        ngos = Institution.objects.filter(type=2)
+        local_collections = Institution.objects.filter(type=3)
         institutions_counter = Donation.objects.distinct('institution_id').count()
         bags_counter = Donation.objects.aggregate(Sum('quantity'))
         return render(request, 'index.html', {'institutions_counter': institutions_counter,
-                                              'bags_counter': bags_counter})
+                                              'bags_counter': bags_counter,
+                                              'funds': funds,
+                                              'ngos': ngos,
+                                              'local_collections': local_collections})
 
 
 class AddDonation(View):
